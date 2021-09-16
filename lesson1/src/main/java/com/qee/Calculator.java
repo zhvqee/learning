1590c4d5db724bb71587cf267ec0475c46d7f602
zhuqi <zhuqi@youzan.com>
2021-09-16 22:14:53 +0800

    Initial commit

---
+++ lesson1/src/main/java/com/qee/Calculator.java
@@ -0,0 +1,38 @@
+package com.qee;
+
+import java.util.function.BiFunction;
+
+/**
+ * @ProjectName: learning
+ * @Package: com.qee
+ * @ClassName: Calculator
+ * @Description:
+ * @Date: 2021/9/14 10:07 上午
+ * @Version: 1.0
+ */
+public class Calculator {
+
+    public static int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multi(int a, int b) {
+        return a * b;
+    }
+
+    public void forEach(int a, int b, BiFunction<Integer, Integer, Integer>... biFunctions) {
+        for (BiFunction bf : biFunctions) {
+            Object c = bf.apply(a, b);
+            System.out.println(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.forEach(100, 200, Calculator::plus, Calculator::subtract, Calculator::multi);
+    }
+}
---
+++ lesson1/src/main/java/com/qee/ClassLoaderTest.java
@@ -0,0 +1,25 @@
+package com.qee;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+
+/**
+ * @ProjectName: learning
+ * @Package: com.qee
+ * @ClassName: ClassLoaderTest
+ * @Description:
+ * @Date: 2021/9/14 8:27 下午
+ * @Version: 1.0
+ */
+public class ClassLoaderTest {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        MyXClassLoader myXClassLoader = new MyXClassLoader(new URL[]{ClassLoaderTest.class.getResource("/")});
+        Class<?> HelloClass = myXClassLoader.loadClass("com.qee.Hello");
+        Method method = HelloClass.getMethod("hello", String.class);
+        Object newInstance = HelloClass.newInstance();
+        method.setAccessible(true);
+        method.invoke(newInstance, "class loader hello world");
+
+    }
+}
